950c867a2eb27bc133258532ae020b0306c1f935
carlton anand <carlton.anand@gmail.com>
2020-10-07 19:41:45 +0100

    creating buttons and the navbar bar functionality, also the css for both

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,39 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true)
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  };
+
+  window.addEventListener('resize', showButton);
+
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-             TRVL <i className="fab fa-typo3"></i>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo'>
+             TRVL
+              <i className='fab fa-typo3'/>
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
             <ul className={click ? 'nav-menu active' : 'name-menu'}>
               <li className='nav-item'>
-                <Link 
-                  to='/' 
-                  className='nav-links' 
-                  onClick={closeMobileMenu}>
+                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                   Home
                 </Link>
               </li>
@@ -50,6 +62,7 @@ function Navbar() {
                 </Link>
               </li>
             </ul>
+            {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
       </nav>
     </>
